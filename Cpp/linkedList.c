8f6a143d95b86530450b72844f5289e38d871afe
tittuthomasgeorgy <tittuthomasgeorgy@gmail.com>
2023-11-27 11:05:03 +0530

    linkedlist

--- Cpp/linkedList.c
+++ Cpp/linkedList.c
@@ -9,6 +9,23 @@ struct Node
 
 struct Node *head = NULL;
 
+void display()
+{
+    if (head == NULL)
+    {
+        printf("Empty list");
+        return;
+    }
+    printf("\n\n  Head: %d ", head);
+    printf("\n List elements:\n");
+    struct Node *tmp = head;
+    while (tmp != NULL)
+    {
+        printf("%d -> ", tmp->data);
+        tmp = tmp->ptr;
+    }
+}
+
 void insertAtBeg()
 {
     int x;
@@ -16,7 +33,10 @@ void insertAtBeg()
     scanf("%d", &x);
     struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
     new_node->data = x;
-    new_node->ptr = head;
+    if (head == NULL)
+        new_node->ptr = NULL;
+    else
+        new_node->ptr = head;
     head = new_node;
     printf("\n Element inserted at Begining");
 }
@@ -32,75 +52,185 @@ void insertAtEnd()
     struct Node *tmp = head;
     while (tmp->ptr != NULL)
         tmp = tmp->ptr;
+    tmp->ptr = new_node;
     printf("\n Element inserted at End");
 }
-
-void dequeue(int a[])
+void insertAtPos()
 {
-    if (isEmpty())
-        return;
-    if (front == rear)
-    {
-        front = -1;
-        rear = -1;
-    }
-    else
-        ++front;
-    printf("\n Element poped:%d,  front: %d, rear: %d  ", a[front], front, rear);
-}
-
-void peek(int a[])
-{
-    if (isEmpty())
-        return;
-    int x;
-    printf("\n Enter position to Peek:");
+    int x, pos, count = 0;
+    printf("\n Enter element to insert:");
     scanf("%d", &x);
-    if (x < front || x > rear)
-        printf("\n !!! INVALID POSITION !!!\n");
-    else
-        printf("\n Element at %d : %d", x, a[x]);
+    printf("\n Enter position to insert:");
+    scanf("%d", &pos);
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->data = x;
+    new_node->ptr = NULL;
+    struct Node *tmp = head;
+    while (count != pos)
+    {
+        tmp = tmp->ptr;
+        count++;
+    }
+    new_node->ptr = tmp->ptr;
+    tmp->ptr = new_node;
+    printf("\n Element inserted at Position %d", &pos);
 }
 
-void display(int a[])
+void insertion()
 {
-    if (isEmpty())
-        return;
-    printf("\n\n  front: %d, rear: %d  ", front, rear);
-    printf("\n Queue elements:\n");
-    for (int i = front; i <= rear; i++)
+    int ch;
+    printf("\n\n 1.Beginning \n 2.End \n 3.Position \n 4.Display \n 5.Return \nEnter your Choice:");
+    scanf("%d", &ch);
+    switch (ch)
     {
-        printf(" %d ", a[i]);
+    case 1:
+    {
+        insertAtBeg();
+        display();
+        break;
+    }
+    case 2:
+    {
+        insertAtEnd();
+        display();
+        break;
+    }
+    case 3:
+    {
+        insertAtPos();
+        display();
+        break;
+    }
+    case 4:
+    {
+        display();
+        break;
+    }
+    case 5:
+    {
+        break;
+    }
+    default:
+        printf("!!! Invalid Choice !!!");
+    }
+}
+void deleteBeg()
+{
+    struct Node *tmp;
+    tmp = head;
+    head = tmp->ptr;
+    free(tmp);
+    printf("\n Element deleted from Begining");
+}
+
+void deleteEnd()
+{
+    struct Node *tmp1 = head;
+    struct Node *tmp2 = NULL;
+    while (tmp1->ptr->ptr != NULL)
+        tmp1 = tmp1->ptr;
+    tmp2 = tmp1->ptr;
+    tmp1->ptr = NULL;
+    printf("\n Element Deleted at End: %d", tmp2->data);
+    free(tmp2);
+}
+void deletePos()
+{
+    int pos, count = 0;
+    printf("\n Enter position to insert:");
+    scanf("%d", &pos);
+    struct Node *tmp = head;
+    while (count < pos)
+    {
+        tmp = tmp->ptr;
+        count++;
+    }
+    struct Node *tmp2 = NULL;
+    tmp2 = tmp->ptr;
+    tmp->ptr = tmp2->ptr;
+    printf("\n Element Deleted at Pos %d : %d  ", pos, tmp2->data);
+    free(tmp2);
+}
+
+void deleteList()
+{
+    int ch;
+    printf("\n\n 1.Beginning \n 2.End \n 3.Position \n 4.Display \n 5.Return \nEnter your Choice:");
+    scanf("%d", &ch);
+    switch (ch)
+    {
+    case 1:
+    {
+        deleteBeg();
+        display();
+        break;
+    }
+    case 2:
+    {
+        deleteEnd();
+        display();
+        break;
+    }
+    case 3:
+    {
+        deletePos();
+        display();
+        break;
+    }
+    case 4:
+    {
+        display();
+        break;
+    }
+    case 5:
+    {
+        break;
+    }
+    default:
+        printf("!!! Invalid Choice !!!");
     }
 }
 
-void main()
+// void peek(int a[])
+// {
+//     if (isEmpty())
+//         return;
+//     int x;
+//     printf("\n Enter position to Peek:");
+//     scanf("%d", &x);
+//     if (x < front || x > rear)
+//         printf("\n !!! INVALID POSITION !!!\n");
+//     else
+//         printf("\n Element at %d : %d", x, a[x]);
+// }
+
+int main()
 {
     int a[10], ch;
     while (1)
     {
-        printf("\n\n 1.Enqueue \n 2.Dequeue \n 3.Peek \n 4.Display \n 5.Exit \nEnter your Choice:");
+        printf("\n\n 1.Push \n 2.Pop \n 3.Peek \n 4.Display \n 5.Exit \nEnter your Choice:");
         scanf("%d", &ch);
         switch (ch)
         {
         case 1:
         {
-            enqueue(a);
+            insertion();
             break;
         }
         case 2:
         {
-            dequeue(a);
+            deleteList();
             break;
         }
         case 3:
         {
-            peek(a);
+            // peek(a);
             break;
         }
         case 4:
         {
-            display(a);
+            display();
             break;
         }
         case 5:
@@ -112,4 +242,5 @@ void main()
             printf("!!! Invalid Choice !!!");
         }
     }
+    return 0;
 }
